0c43c58ff9506203285a95743fb0e8646b9befed
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-31 21:11:12 +0100

    add a change location button

--- public/app.js
+++ public/app.js
@@ -1,13 +1,17 @@
 const app = function() {
 
-  let coords = [-37.813628, 144.963058];
-  let coords2 = [-37.773772, 144.958497];
-  let zoom = 8;
+  let coords = [-37.776311, 144.971159];
+  let ibrox = [55.853177, -4.309265];
+  let zoom = 15;
   let containerID = 'main-map';
 
   const mainMap = new MapWrapper(containerID, coords, zoom);
 
-  
+  var locationButton = document.querySelector('#loc-button');
+  locationButton.addEventListener('click', function(){
+    mainMap.changeLocation(ibrox);
+    mainMap.addMarker(ibrox);
+  })
 
 }
 
